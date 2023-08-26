802ccabd518ab6343b9d227f4e070127ac06ccf7
sarfrazali78 <80842669+sarfrazali78@users.noreply.github.com>
2023-08-26 16:14:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Key from "./Components/Home";
+
+export default function App() {
+  return (
+    <div className="App">
+     <Key/>
+    </div>
+  );
+}
---
+++ src/Components/Home.jsx
@@ -0,0 +1,61 @@
+import React from "react"
+import './Home.css'
+
+export default function Key(){
+  return(
+    <div>
+    <div className='container'>
+
+      <div className='card'>
+      <img src="https://images.pexels.com/photos/16741654/pexels-photo-16741654/free-photo-of-sea-landscape-man-beach.jpeg?height=400px & width=400px" alt="" />
+       </div>
+
+      
+          
+       <div className='card'>
+      <img src="https://images.pexels.com/photos/8387170/pexels-photo-8387170.jpeg?height=400px & width=400px" alt="" />
+       </div>
+
+       <div className='card'>
+      <img src="https://images.pexels.com/photos/17727510/pexels-photo-17727510/free-photo-of-processed-with-vsco-with-ka1-preset.jpeg?height=400px & width=400px" alt="" />
+       </div>
+
+       <div className='card'>
+      <img src="https://images.pexels.com/photos/18122663/pexels-photo-18122663.png?height=400px & width=400px" alt=""/>
+       </div> 
+
+    </div>
+
+    <div className='heading'>
+    <h1>Meet Your Mentors</h1>
+    </div>
+
+    <div className='container1'>
+
+      <div className='cardSlide'> 
+      <img src="https://images.pexels.com/photos/7092613/pexels-photo-7092613.jpeg?height=400px & width=400px" alt="" />
+         </div> 
+
+         <div className='cardSlide'> 
+      <img src="https://images.pexels.com/photos/6334771/pexels-photo-6334771.jpeg?height=400px & width=400px" alt="" />
+         </div> 
+
+         <div className='cardSlide'> 
+      <img src="https://images.pexels.com/photos/5905898/pexels-photo-5905898.jpeg?height=400px & width=400px" alt="" />
+         </div> 
+
+         <div className='cardSlide'> 
+      <img src="https://images.pexels.com/photos/6326377/pexels-photo-6326377.jpeg?height=400px & width=400px" alt="" />
+         </div> 
+
+
+    </div>
+
+
+
+
+    </div>
+
+   
+  )
+}
